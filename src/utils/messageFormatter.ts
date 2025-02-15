c1d92c6c23717b2d0a658450d7aa760a0ca2ea04
Godwin Kimani <josephgodwinke@yandex.com>
2025-02-15 09:10:19 +0300

    add usage

--- src/utils/messageFormatter.ts
+++ src/utils/messageFormatter.ts
@@ -1,16 +1,15 @@
 import { marked } from 'marked';
 import Prism from 'prismjs';
-import 'prismjs/themes/prism-tomorrow.css';
-import 'prismjs/components/prism-typescript';
-import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-go';
+import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-json';
 import 'prismjs/components/prism-python';
-import 'prismjs/components/prism-go';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/themes/prism-tomorrow.css';
 
-// Configure marked options
 marked.setOptions({
-  highlight: function (code, lang) {
+  highlight: function (code: string, lang: string) {
     if (Prism.languages[lang]) {
       return Prism.highlight(code, Prism.languages[lang], lang);
     }
